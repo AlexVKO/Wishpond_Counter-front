73d19f75a9fd1f1ee3e8068e74b2f72bfaa10863
Alex VKO <ale@alexvko.com>
2017-03-02 20:22:13 -0300

    Starts project with Angular 1.4.0

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webApp
+ * @description
+ * # webApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webApp', [
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
